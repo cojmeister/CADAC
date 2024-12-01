5e3b922298783474a4c1c6155b0164b7eebea8bc
cojmeister <luqas.c@gmail.com>
2024-12-01 15:59:20 +0200

    Initial commit

---
+++ cmake-build-debug/GHAME3/CMakeFiles/GHAME3.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/class_functions.cpp" "GHAME3/CMakeFiles/GHAME3.dir/class_functions.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/class_functions.cpp.o.d"
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/cruise_functions.cpp" "GHAME3/CMakeFiles/GHAME3.dir/cruise_functions.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/cruise_functions.cpp.o.d"
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/cruise_modules.cpp" "GHAME3/CMakeFiles/GHAME3.dir/cruise_modules.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/cruise_modules.cpp.o.d"
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/execution.cpp" "GHAME3/CMakeFiles/GHAME3.dir/execution.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/execution.cpp.o.d"
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/global_functions.cpp" "GHAME3/CMakeFiles/GHAME3.dir/global_functions.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/global_functions.cpp.o.d"
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/round3_modules.cpp" "GHAME3/CMakeFiles/GHAME3.dir/round3_modules.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/round3_modules.cpp.o.d"
+  "/Users/lucascohen/Documents/Projects/CADAC/GHAME3/utility_functions.cpp" "GHAME3/CMakeFiles/GHAME3.dir/utility_functions.cpp.o" "gcc" "GHAME3/CMakeFiles/GHAME3.dir/utility_functions.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
